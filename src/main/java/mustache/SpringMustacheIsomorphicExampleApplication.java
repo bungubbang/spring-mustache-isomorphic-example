a2cd355df1baf8f2c915d7a3f945d05ef744295f
bungubbang <bub4419>
2015-10-06 16:36:36 +0900

    init spring boot

---
+++ src/main/java/mustache/SpringMustacheIsomorphicExampleApplication.java
@@ -0,0 +1,12 @@
+package mustache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMustacheIsomorphicExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMustacheIsomorphicExampleApplication.class, args);
+    }
+}
